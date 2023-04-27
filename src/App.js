71acf9ed36493c6ccae63233d81d7f6b8366c405
Daniel <danigutierrez913@gmail.com>
2023-04-26 23:01:15 -0300

    se agrega el componente itemlist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Itemslist from './Components/Items/Itemslist';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Itemslist/>
     
     </div>
   );
---
+++ src/Components/Items/Itemslist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Itemslist = () => {
+  return (
+    <div>Itemslist</div>
+  )
+}
+
+export default Itemslist
